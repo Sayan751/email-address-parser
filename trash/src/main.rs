ee0b78e0da2467a2fa34df335406a775c0277432
Sayan751 <sayan751@gmail.com>
2020-08-30 00:04:29 +0200

    cleanup

--- trash/src/main.rs
+++
@@ -1,7 +0,0 @@
-// mod email_address_parser;
-use email_address_parser::email_address::EmailAddress;
-
-fn main() {
-    assert!(EmailAddress::parse("foo@bar.com", Some(true)).is_some());
-    assert!(EmailAddress::parse("foo@-bar.com", Some(true)).is_none());
-}
