f001cbaad62f4c7f49f6adde1c1c7b7d82387996
Joshua Clayton <joshua.clayton@gmail.com>
2021-04-14 21:47:03 -0400

    rustfmt

--- rust-lib/benches/benchmarks.rs
+++ rust-lib/benches/benchmarks.rs
@@ -5,34 +5,93 @@ const EMAIL: &str = "foo@bar.com";
 const EMAIL_INVALID_LOCAL_PART: &str = "foo-@bar.com";
 const EMAIL_INVALID_DOMAIN: &str = "foo@-bar.com";
 const EMAIL_UNICODE: &str = "foö@bär.com";
-const EMAIL_LONG: &str = "this.is.a.very-long.email@super-super.deliberately.long.and.awesome-domain.com";
+const EMAIL_LONG: &str =
+    "this.is.a.very-long.email@super-super.deliberately.long.and.awesome-domain.com";
 const EMAIL_OBS: &str = "\u{0d}\u{0a} \u{0d}\u{0a} test@iana.org";
 
 pub fn parse_benchmark(c: &mut Criterion) {
-  c.bench_function("parse - valid", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL), black_box(None))));
-  c.bench_function("parse - invalid local part", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL_INVALID_LOCAL_PART), black_box(None))));
-  c.bench_function("parse - invalid domain", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL_INVALID_DOMAIN), black_box(None))));
-  c.bench_function("parse - unicode", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL_UNICODE), black_box(None))));
-  c.bench_function("parse - long", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL_LONG), black_box(None))));
-  c.bench_function("parse - obs", |b| b.iter(|| EmailAddress::parse(black_box(EMAIL_OBS), black_box(Some(ParsingOptions::new(true))))));
+    c.bench_function("parse - valid", |b| {
+        b.iter(|| EmailAddress::parse(black_box(EMAIL), black_box(None)))
+    });
+    c.bench_function("parse - invalid local part", |b| {
+        b.iter(|| EmailAddress::parse(black_box(EMAIL_INVALID_LOCAL_PART), black_box(None)))
+    });
+    c.bench_function("parse - invalid domain", |b| {
+        b.iter(|| EmailAddress::parse(black_box(EMAIL_INVALID_DOMAIN), black_box(None)))
+    });
+    c.bench_function("parse - unicode", |b| {
+        b.iter(|| EmailAddress::parse(black_box(EMAIL_UNICODE), black_box(None)))
+    });
+    c.bench_function("parse - long", |b| {
+        b.iter(|| EmailAddress::parse(black_box(EMAIL_LONG), black_box(None)))
+    });
+    c.bench_function("parse - obs", |b| {
+        b.iter(|| {
+            EmailAddress::parse(
+                black_box(EMAIL_OBS),
+                black_box(Some(ParsingOptions::new(true))),
+            )
+        })
+    });
 }
 
 pub fn is_valid_benchmark(c: &mut Criterion) {
-  c.bench_function("is_valid - valid", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL), black_box(None))));
-  c.bench_function("is_valid - invalid local part", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL_INVALID_LOCAL_PART), black_box(None))));
-  c.bench_function("is_valid - invalid domain", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL_INVALID_DOMAIN), black_box(None))));
-  c.bench_function("is_valid - unicode", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL_UNICODE), black_box(None))));
-  c.bench_function("is_valid - long", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL_LONG), black_box(None))));
-  c.bench_function("is_valid - obs", |b| b.iter(|| EmailAddress::is_valid(black_box(EMAIL_OBS), black_box(Some(ParsingOptions::new(true))))));
+    c.bench_function("is_valid - valid", |b| {
+        b.iter(|| EmailAddress::is_valid(black_box(EMAIL), black_box(None)))
+    });
+    c.bench_function("is_valid - invalid local part", |b| {
+        b.iter(|| EmailAddress::is_valid(black_box(EMAIL_INVALID_LOCAL_PART), black_box(None)))
+    });
+    c.bench_function("is_valid - invalid domain", |b| {
+        b.iter(|| EmailAddress::is_valid(black_box(EMAIL_INVALID_DOMAIN), black_box(None)))
+    });
+    c.bench_function("is_valid - unicode", |b| {
+        b.iter(|| EmailAddress::is_valid(black_box(EMAIL_UNICODE), black_box(None)))
+    });
+    c.bench_function("is_valid - long", |b| {
+        b.iter(|| EmailAddress::is_valid(black_box(EMAIL_LONG), black_box(None)))
+    });
+    c.bench_function("is_valid - obs", |b| {
+        b.iter(|| {
+            EmailAddress::is_valid(
+                black_box(EMAIL_OBS),
+                black_box(Some(ParsingOptions::new(true))),
+            )
+        })
+    });
 }
 
 pub fn new_benchmark(c: &mut Criterion) {
-  c.bench_function("new - valid", |b| b.iter(|| EmailAddress::new(black_box("foo"), black_box("bar.com"), black_box(None))));
-  c.bench_function("new - invalid local part", |b| b.iter(|| EmailAddress::new(black_box("foo-"), black_box("bar.com"), black_box(None))));
-  c.bench_function("new - invalid domain", |b| b.iter(|| EmailAddress::new(black_box("foo"), black_box("-bar.com"), black_box(None))));
-  c.bench_function("new - unicode", |b| b.iter(|| EmailAddress::new(black_box("foö"), black_box("bär.com"), black_box(None))));
-  c.bench_function("new - long", |b| b.iter(|| EmailAddress::new(black_box("this.is.a.very-long.email"), black_box("super-super.deliberately.long.and.awesome-domain.com"), black_box(None))));
-  c.bench_function("new - obs", |b| b.iter(|| EmailAddress::new(black_box("\u{0d}\u{0a} \u{0d}\u{0a} test"), black_box("iana.org"), black_box(None))));
+    c.bench_function("new - valid", |b| {
+        b.iter(|| EmailAddress::new(black_box("foo"), black_box("bar.com"), black_box(None)))
+    });
+    c.bench_function("new - invalid local part", |b| {
+        b.iter(|| EmailAddress::new(black_box("foo-"), black_box("bar.com"), black_box(None)))
+    });
+    c.bench_function("new - invalid domain", |b| {
+        b.iter(|| EmailAddress::new(black_box("foo"), black_box("-bar.com"), black_box(None)))
+    });
+    c.bench_function("new - unicode", |b| {
+        b.iter(|| EmailAddress::new(black_box("foö"), black_box("bär.com"), black_box(None)))
+    });
+    c.bench_function("new - long", |b| {
+        b.iter(|| {
+            EmailAddress::new(
+                black_box("this.is.a.very-long.email"),
+                black_box("super-super.deliberately.long.and.awesome-domain.com"),
+                black_box(None),
+            )
+        })
+    });
+    c.bench_function("new - obs", |b| {
+        b.iter(|| {
+            EmailAddress::new(
+                black_box("\u{0d}\u{0a} \u{0d}\u{0a} test"),
+                black_box("iana.org"),
+                black_box(None),
+            )
+        })
+    });
 }
 
 criterion_group!(benches, parse_benchmark, is_valid_benchmark, new_benchmark);
--- rust-lib/build.rs
+++ rust-lib/build.rs
@@ -6,89 +6,89 @@ use std::io::Write;
 use std::path;
 
 fn main() {
-  // cargo env var reference: https://doc.rust-lang.org/cargo/reference/environment-variables.html
-  let root = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-  let out_dir = env::var_os("OUT_DIR").unwrap();
-  let test_file_path = path::Path::new(&out_dir).join("generated_tests.rs");
-  let mut test_file = fs::File::create(&test_file_path).unwrap();
-  let test_data_root = path::Path::new(&root).join(".test_data");
+    // cargo env var reference: https://doc.rust-lang.org/cargo/reference/environment-variables.html
+    let root = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let test_file_path = path::Path::new(&out_dir).join("generated_tests.rs");
+    let mut test_file = fs::File::create(&test_file_path).unwrap();
+    let test_data_root = path::Path::new(&root).join(".test_data");
 
-  let read_test_data = |file_name: &str| {
-    fs::read_to_string(test_data_root.join(file_name))
-      .expect(&format!("{} not found", file_name))
-      .lines()
-      .map(|v| String::from(v))
-      .collect()
-  };
+    let read_test_data = |file_name: &str| {
+        fs::read_to_string(test_data_root.join(file_name))
+            .expect(&format!("{} not found", file_name))
+            .lines()
+            .map(|v| String::from(v))
+            .collect()
+    };
 
-  let valid_local_parts: Vec<String> = read_test_data("valid_local_parts.txt");
-  let valid_domains: Vec<String> = read_test_data("valid_domains.txt");
-  let invalid_local_parts: Vec<String> = read_test_data("invalid_local_parts.txt");
-  let invalid_domains: Vec<String> = read_test_data("invalid_domains.txt");
+    let valid_local_parts: Vec<String> = read_test_data("valid_local_parts.txt");
+    let valid_domains: Vec<String> = read_test_data("valid_domains.txt");
+    let invalid_local_parts: Vec<String> = read_test_data("invalid_local_parts.txt");
+    let invalid_domains: Vec<String> = read_test_data("invalid_domains.txt");
 
-  let mut content = String::new();
-  create_valid_parsing_tests(&mut content, &valid_local_parts, &valid_domains);
-  create_invalid_parsing_tests(
-    &mut content,
-    &valid_local_parts,
-    &valid_domains,
-    &invalid_local_parts,
-    &invalid_domains,
-  );
+    let mut content = String::new();
+    create_valid_parsing_tests(&mut content, &valid_local_parts, &valid_domains);
+    create_invalid_parsing_tests(
+        &mut content,
+        &valid_local_parts,
+        &valid_domains,
+        &invalid_local_parts,
+        &invalid_domains,
+    );
 
-  create_is_email_tests(&mut content, &test_data_root);
+    create_is_email_tests(&mut content, &test_data_root);
 
-  create_valid_instantiation_tests(&mut content, &valid_local_parts, &valid_domains);
-  create_invalid_instantiation_tests(
-    &mut content,
-    &valid_local_parts,
-    &valid_domains,
-    &invalid_local_parts,
-    &invalid_domains,
-  );
+    create_valid_instantiation_tests(&mut content, &valid_local_parts, &valid_domains);
+    create_invalid_instantiation_tests(
+        &mut content,
+        &valid_local_parts,
+        &valid_domains,
+        &invalid_local_parts,
+        &invalid_domains,
+    );
 
-  create_is_valid_tests(
-    &mut content,
-    &valid_local_parts,
-    &valid_domains,
-    &invalid_local_parts,
-    &invalid_domains,
-  );
+    create_is_valid_tests(
+        &mut content,
+        &valid_local_parts,
+        &valid_domains,
+        &invalid_local_parts,
+        &invalid_domains,
+    );
 
-  write!(test_file, "{}", content.trim()).unwrap();
-  println!("cargo:rerun-if-changed=build.rs");
-  println!("cargo:rerun-if-changed=resources/.test_data/valid_local_parts.txt");
-  println!("cargo:rerun-if-changed=resources/.test_data/valid_domains.txt");
-  println!("cargo:rerun-if-changed=resources/.test_data/invalid_local_parts.txt");
-  println!("cargo:rerun-if-changed=resources/.test_data/invalid_domains.txt");
+    write!(test_file, "{}", content.trim()).unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=resources/.test_data/valid_local_parts.txt");
+    println!("cargo:rerun-if-changed=resources/.test_data/valid_domains.txt");
+    println!("cargo:rerun-if-changed=resources/.test_data/invalid_local_parts.txt");
+    println!("cargo:rerun-if-changed=resources/.test_data/invalid_domains.txt");
 }
 
 fn create_case(
-  content: &mut String,
-  case_index: &mut i32,
-  local_parts: &Vec<String>,
-  domains: &Vec<String>,
+    content: &mut String,
+    case_index: &mut i32,
+    local_parts: &Vec<String>,
+    domains: &Vec<String>,
 ) {
-  for local_part in local_parts {
-    for domain in domains {
-      *case_index += 1;
-      content.push_str(&format!(
-        "  {}: (\"{}\", \"{}\"),\n",
-        &format!("case{}", case_index),
-        local_part,
-        domain
-      ));
+    for local_part in local_parts {
+        for domain in domains {
+            *case_index += 1;
+            content.push_str(&format!(
+                "  {}: (\"{}\", \"{}\"),\n",
+                &format!("case{}", case_index),
+                local_part,
+                domain
+            ));
+        }
     }
-  }
 }
 
 fn create_valid_parsing_tests(
-  content: &mut String,
-  local_parts: &Vec<String>,
-  domains: &Vec<String>,
+    content: &mut String,
+    local_parts: &Vec<String>,
+    domains: &Vec<String>,
 ) {
-  content.push_str(
-    "
+    content.push_str(
+        "
 macro_rules! generate_positive_parsing_test {
   ($($case:ident: ($local_part:literal, $domain:literal),)+) => {
     #[cfg(test)]
@@ -115,20 +115,20 @@ macro_rules! generate_positive_parsing_test {
 
 generate_positive_parsing_test!{
 ",
-  );
-  create_case(content, &mut 0, local_parts, domains);
+    );
+    create_case(content, &mut 0, local_parts, domains);
 
-  content.push_str("}\n");
+    content.push_str("}\n");
 }
 
 fn create_invalid_parsing_tests(
-  content: &mut String,
-  valid_local_parts: &Vec<String>,
-  valid_domains: &Vec<String>,
-  invalid_local_parts: &Vec<String>,
-  invalid_domains: &Vec<String>,
+    content: &mut String,
+    valid_local_parts: &Vec<String>,
+    valid_domains: &Vec<String>,
+    invalid_local_parts: &Vec<String>,
+    invalid_domains: &Vec<String>,
 ) {
-  content.push_str(
+    content.push_str(
     "
 macro_rules! generate_negative_parsing_test {
   ($($case:ident: ($local_part:literal, $domain:literal),)+) => {
@@ -152,18 +152,18 @@ macro_rules! generate_negative_parsing_test {
 generate_negative_parsing_test!{
 ",
   );
-  let mut i = 0;
-  create_case(content, &mut i, invalid_local_parts, valid_domains);
-  create_case(content, &mut i, valid_local_parts, invalid_domains);
-  create_case(content, &mut i, invalid_local_parts, invalid_domains);
+    let mut i = 0;
+    create_case(content, &mut i, invalid_local_parts, valid_domains);
+    create_case(content, &mut i, valid_local_parts, invalid_domains);
+    create_case(content, &mut i, invalid_local_parts, invalid_domains);
 
-  content.push_str("}\n");
+    content.push_str("}\n");
 }
 
 fn create_is_email_tests(content: &mut String, test_data_root: &path::Path) {
-  let is_email_xml = fs::read_to_string(test_data_root.join("isemail_tests.xml")).unwrap();
+    let is_email_xml = fs::read_to_string(test_data_root.join("isemail_tests.xml")).unwrap();
 
-  content.push_str(
+    content.push_str(
     "
 macro_rules! generate_is_email_test {
   ($($case:ident: ($email:literal, $is_email:literal),)+) => {
@@ -195,81 +195,81 @@ generate_is_email_test!{
 ",
   );
 
-  let mut reader = Reader::from_str(&is_email_xml);
+    let mut reader = Reader::from_str(&is_email_xml);
 
-  let mut buf = Vec::new();
-  let mut email: String = String::new();
-  let mut is_valid: bool = false;
-  let mut capture: String = String::new();
-  let mut should_capture = false;
-  let mut i = 0;
-  let ignored_emails: Vec<&str> = vec![
-    r"test@[RFC-5322-\\\u{09}-domain-literal]",
-    r"test@[RFC-5322-\\\u{07}-domain-literal]",
-    r"test@[RFC-5322-\\]-domain-literal]",
-  ];
+    let mut buf = Vec::new();
+    let mut email: String = String::new();
+    let mut is_valid: bool = false;
+    let mut capture: String = String::new();
+    let mut should_capture = false;
+    let mut i = 0;
+    let ignored_emails: Vec<&str> = vec![
+        r"test@[RFC-5322-\\\u{09}-domain-literal]",
+        r"test@[RFC-5322-\\\u{07}-domain-literal]",
+        r"test@[RFC-5322-\\]-domain-literal]",
+    ];
 
-  loop {
-    match reader.read_event(&mut buf) {
-      Ok(Event::Start(ref e)) => {
-        should_capture = match e.name() {
-          b"address" | b"category" => true,
-          _ => false,
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                should_capture = match e.name() {
+                    b"address" | b"category" => true,
+                    _ => false,
+                }
+            }
+            Ok(Event::Text(e)) => {
+                capture = if should_capture {
+                    e.unescape_and_decode(&reader).unwrap()
+                } else {
+                    String::new()
+                }
+            }
+            Ok(Event::End(ref e)) => match e.name() {
+                b"address" => {
+                    email = capture
+                        .clone()
+                        .replace("\\", "\\\\")
+                        .replace("\"", "\\\"")
+                        .replace("\u{240D}", "\\u{0d}")
+                        .replace("\u{240A}", "\\u{0a}")
+                        .replace("\u{2400}", "\\u{00}")
+                        .replace("\u{2407}", "\\u{07}")
+                        .replace("\u{2409}", "\\u{09}")
+                }
+                b"category" => {
+                    is_valid = capture != "ISEMAIL_ERR";
+                }
+                b"test" => {
+                    i += 1;
+                    if !ignored_emails.contains(&email.as_str()) {
+                        content.push_str(&format!(
+                            "  {}: (\"{}\", {}),\n",
+                            &format!("case{}", i),
+                            email,
+                            is_valid,
+                        ));
+                    }
+                }
+                _ => (),
+            },
+            Ok(Event::Eof) => break, // exits the loop when reaching end of file
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (), // There are several other `Event`s we do not consider here
         }
-      }
-      Ok(Event::Text(e)) => {
-        capture = if should_capture {
-          e.unescape_and_decode(&reader).unwrap()
-        } else {
-          String::new()
-        }
-      }
-      Ok(Event::End(ref e)) => match e.name() {
-        b"address" => {
-          email = capture
-            .clone()
-            .replace("\\", "\\\\")
-            .replace("\"", "\\\"")
-            .replace("\u{240D}", "\\u{0d}")
-            .replace("\u{240A}", "\\u{0a}")
-            .replace("\u{2400}", "\\u{00}")
-            .replace("\u{2407}", "\\u{07}")
-            .replace("\u{2409}", "\\u{09}")
-        }
-        b"category" => {
-          is_valid = capture != "ISEMAIL_ERR";
-        }
-        b"test" => {
-          i += 1;
-          if !ignored_emails.contains(&email.as_str()) {
-            content.push_str(&format!(
-              "  {}: (\"{}\", {}),\n",
-              &format!("case{}", i),
-              email,
-              is_valid,
-            ));
-          }
-        }
-        _ => (),
-      },
-      Ok(Event::Eof) => break, // exits the loop when reaching end of file
-      Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-      _ => (), // There are several other `Event`s we do not consider here
+
+        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        buf.clear();
     }
 
-    // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
-    buf.clear();
-  }
-
-  content.push_str("}");
+    content.push_str("}");
 }
 
 fn create_valid_instantiation_tests(
-  content: &mut String,
-  local_parts: &Vec<String>,
-  domains: &Vec<String>,
+    content: &mut String,
+    local_parts: &Vec<String>,
+    domains: &Vec<String>,
 ) {
-  content.push_str(
+    content.push_str(
     "
 macro_rules! generate_positive_instantiation_test {
   ($($case:ident: ($local_part:literal, $domain:literal),)+) => {
@@ -295,19 +295,19 @@ macro_rules! generate_positive_instantiation_test {
 generate_positive_instantiation_test!{
 ",
   );
-  create_case(content, &mut 0, local_parts, domains);
+    create_case(content, &mut 0, local_parts, domains);
 
-  content.push_str("}\n");
+    content.push_str("}\n");
 }
 
 fn create_invalid_instantiation_tests(
-  content: &mut String,
-  valid_local_parts: &Vec<String>,
-  valid_domains: &Vec<String>,
-  invalid_local_parts: &Vec<String>,
-  invalid_domains: &Vec<String>,
+    content: &mut String,
+    valid_local_parts: &Vec<String>,
+    valid_domains: &Vec<String>,
+    invalid_local_parts: &Vec<String>,
+    invalid_domains: &Vec<String>,
 ) {
-  content.push_str(
+    content.push_str(
     "
 macro_rules! generate_negative_instantiation_test {
   ($($case:ident: ($local_part:literal, $domain:literal),)+) => {
@@ -330,41 +330,41 @@ macro_rules! generate_negative_instantiation_test {
 generate_negative_instantiation_test!{
 ",
   );
-  let mut i = 0;
-  create_case(content, &mut i, invalid_local_parts, valid_domains);
-  create_case(content, &mut i, valid_local_parts, invalid_domains);
-  create_case(content, &mut i, invalid_local_parts, invalid_domains);
+    let mut i = 0;
+    create_case(content, &mut i, invalid_local_parts, valid_domains);
+    create_case(content, &mut i, valid_local_parts, invalid_domains);
+    create_case(content, &mut i, invalid_local_parts, invalid_domains);
 
-  content.push_str("}\n");
+    content.push_str("}\n");
 }
 
 fn create_is_valid_tests(
-  content: &mut String,
-  valid_local_parts: &Vec<String>,
-  valid_domains: &Vec<String>,
-  invalid_local_parts: &Vec<String>,
-  invalid_domains: &Vec<String>,
-) {
-  fn create_is_valid_case(
     content: &mut String,
-    case_index: &mut i32,
-    local_parts: &Vec<String>,
-    domains: &Vec<String>,
-    is_valid: bool,
-  ) {
-    for local_part in local_parts {
-      for domain in domains {
-        *case_index += 1;
-        content.push_str(&format!(
-          "  {}: (\"{}\", {}),\n",
-          &format!("case{}", case_index),
-          format!("{}@{}", local_part, domain),
-          is_valid
-        ));
-      }
+    valid_local_parts: &Vec<String>,
+    valid_domains: &Vec<String>,
+    invalid_local_parts: &Vec<String>,
+    invalid_domains: &Vec<String>,
+) {
+    fn create_is_valid_case(
+        content: &mut String,
+        case_index: &mut i32,
+        local_parts: &Vec<String>,
+        domains: &Vec<String>,
+        is_valid: bool,
+    ) {
+        for local_part in local_parts {
+            for domain in domains {
+                *case_index += 1;
+                content.push_str(&format!(
+                    "  {}: (\"{}\", {}),\n",
+                    &format!("case{}", case_index),
+                    format!("{}@{}", local_part, domain),
+                    is_valid
+                ));
+            }
+        }
     }
-  }
-  content.push_str(
+    content.push_str(
     "
 macro_rules! generate_is_valid_test {
   ($($case:ident: ($address:literal, $is_valid:literal),)+) => {
@@ -387,11 +387,11 @@ macro_rules! generate_is_valid_test {
 generate_is_valid_test!{
 ",
   );
-  let mut i = 0;
-  create_is_valid_case(content, &mut i, valid_local_parts, valid_domains, true);
-  create_is_valid_case(content, &mut i, invalid_local_parts, valid_domains, false);
-  create_is_valid_case(content, &mut i, valid_local_parts, invalid_domains, false);
-  create_is_valid_case(content, &mut i, invalid_local_parts, invalid_domains, false);
+    let mut i = 0;
+    create_is_valid_case(content, &mut i, valid_local_parts, valid_domains, true);
+    create_is_valid_case(content, &mut i, invalid_local_parts, valid_domains, false);
+    create_is_valid_case(content, &mut i, valid_local_parts, invalid_domains, false);
+    create_is_valid_case(content, &mut i, invalid_local_parts, invalid_domains, false);
 
-  content.push_str("}\n");
+    content.push_str("}\n");
 }
